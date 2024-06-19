95bd316e199a3b6c42edf481c211d1a48d04b929
Lucas Queiroz <me@lucasqueiroz.dev>
2024-06-19 08:08:40 -0300

    nvim

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -22,32 +22,6 @@ return {
           hide_gitignored = false,
         },
       },
-      event_handlers = {
-        {
-          -- Open file when creating
-          event = "file_added",
-          handler = function(arg)
-            vim.cmd("edit " .. arg)
-            vim.cmd "start"
-          end,
-        },
-        {
-          event = "neo_tree_buffer_enter",
-          handler = function(_)
-            vim.cmd.amenu [[PopUp.New\ File a]]
-            vim.cmd.amenu [[PopUp.Rename r]]
-            vim.cmd.amenu [[PopUp.Delete d]]
-          end,
-        },
-        {
-          event = "neo_tree_buffer_leave",
-          handler = function(_)
-            vim.cmd.aunmenu [[PopUp.New\ File]]
-            vim.cmd.aunmenu [[PopUp.Rename]]
-            vim.cmd.aunmenu [[PopUp.Delete]]
-          end,
-        },
-      },
     },
   },
 }
