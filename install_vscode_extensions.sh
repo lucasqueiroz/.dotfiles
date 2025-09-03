bf54a4c7c103397a00fdd221e81acddc40dfc53c
Lucas Queiroz <hello@lucasqueiroz.dev>
2025-09-02 22:50:30 -0300

    202509022250

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.vscode-python-envs
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension Catppuccin.catppuccin-vsc
+code --install-extension Catppuccin.catppuccin-vsc-icons
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-python.isort
+code --install-extension batisteo.vscode-django
+code --install-extension charliermarsh.ruff
+code --install-extension matangover.mypy
+code --install-extension ms-python.black-formatter
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension LittleFoxTeam.vscode-python-test-adapter
+code --install-extension tamasfe.even-better-toml
+code --install-extension redhat.vscode-yaml
+code --install-extension ecmel.vscode-html-css
