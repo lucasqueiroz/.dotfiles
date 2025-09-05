9ebe4550568741ff642062afb64cac733d2d0b51
Lucas Queiroz <hello@lucasqueiroz.dev>
2025-09-05 03:14:01 -0300

    2025100313

--- install_ubuntu_pkgs.sh
+++ install_ubuntu_pkgs.sh
@@ -75,6 +75,9 @@ nix-env -iA nixpkgs.solaar
 # NVim
 nix-env -iA nixpkgs.neovim
 
+# RClone
+nix-env -iA nixpkgs.rclone
+
 # ProtonVPN
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
 sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
